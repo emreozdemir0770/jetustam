cbce2fde40a32dbb6c100922b4b4512e30526234
emreozdemir0770 <emre.ozdemir.0770gmail.com>
2020-12-28 00:04:52 +0300

    config clear

--- protected/config/main.php
+++ protected/config/main.php
@@ -71,10 +71,10 @@ return array(
 										
 	    'db'=>array(	        
 		    'class'            => 'CDbConnection' ,
-			'connectionString' => 'mysql:host=localhost;dbname=jetustam',
+			'connectionString' => 'mysql:host=localhost;dbname=emreozd1_test',
 			'emulatePrepare'   => true,
-			'username'         => 'root',
-			'password'         => 'root',
+			'username'         => 'emreozd1_wp810',
+			'password'         => 'W8NJ!5*sy@wm',
 			'charset'          => 'utf8',
 			'tablePrefix'      => 'mtt_',
 	    ),
