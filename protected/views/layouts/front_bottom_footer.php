edb3898c4d8aeaa2afafb7a452118269be464fa4
emreozdemir0770 <emre.ozdemir.0770gmail.com>
2021-01-02 03:33:37 +0300

    email kaldırıldı

--- protected/views/layouts/front_bottom_footer.php
+++ protected/views/layouts/front_bottom_footer.php
@@ -3,26 +3,6 @@
 <form method="POST" id="frm-subscribe" class="frm-subscribe" onsubmit="return false;">
 <?php echo CHtml::hiddenField('action','subscribeNewsletter')?>
 <div class="sections section-subcribe">
-  <div class="container">
-      <h2><?php echo t("Haber bültenimize abone ol") ?></h2>
-      <div class="subscribe-footer">
-          <div class="row border">
-             <div class="col-md-3 border col-md-offset-4 ">
-               <?php echo CHtml::textField('subscriber_email','',array(
-                 'placeholder'=>t("E-mail"),
-                 'required'=>true,
-                 'class'=>"email"
-               ))?>
-             </div>
-             <div class="col-md-2 border">
-               <button class="green-button rounded">
-                <?php echo t("Subscribe")?>
-               </button>               
-             </div>
-          </div>
-      </div>
-  </div>
-  
 
 <img src="<?php echo assetsURL()."/images/divider.png"?>" class="footer-divider">
   
